56f91e7cb8a27aa612d871a568a30b93708a69cf
Dan Roberts <dan@adadevelopersacademy.org>
2018-02-10 19:18:03 -0800

    Final version

--- table_formatting.zsh
+++ table_formatting.zsh
@@ -1,42 +1,49 @@
 #!env zsh
 
+echo_time() {
+  date +"%Y-%m-%d %H:%M:%S $*"
+}
+
 if [[ -d .git ]] ; then
-  echo "You are currently in a git repo! I need an empty directory to work in - please make one for me and run me again."
+  echo_time "You are currently in a git repo! I need an empty directory to work in - please make one for me and run me again."
   exit 1
 fi
 
-LOCATION=${0%/*}
+LOCATION=`greadlink -f ${0%/*}`
 PROJECT_LIST_FILE="$LOCATION/projects.txt"
-SED_EXPRESSION='s/^\|\s*([^\|]*[^ \t\|])\s*\|\s*([^\|]*[^ \t\|])?\s*\|\s*$/\1 | \2/g'
-FILES_TO_CONVERT=('feedback.md' 'PULL_REQUEST_TEMPLATE' '.github/PULL_REQUEST_TEMPLATE')
 COMMIT_MESSAGE="$LOCATION/commit_message.txt"
+SED_EXPRESSION='s/^\|\s*([^\|]*[^ \t\|])\s*\|\s*([^\|]*[^ \t\|])?\s*\|\s*$/\1 | \2/g'
+FILES_TO_CONVERT=('feedback.md' 'PULL_REQUEST_TEMPLATE' 'PULL_REQUEST_TEMPLATE.md' '.github/PULL_REQUEST_TEMPLATE' '.github/PULL_REQUEST_TEMPLATE.md')
 
-
-echo "Converting to concise table formatting!"
+echo_time "Converting to concise table formatting!"
 
 for PROJECT in `tail -r $PROJECT_LIST_FILE | grep -v "^#"`; do
-  echo
+  echo_time
+
+  if [[ -d $PROJECT ]] ; then
+    echo_time "Folder $PROJECT already exists. I will not touch it."
+  fi
 
   REPO="git@github.com:AdaGold/$PROJECT.git"
-  echo "Converting $PROJECT in repo $REPO"
+  echo_time "Converting $PROJECT in repo $REPO"
 
   git clone $REPO
   cd $PROJECT
 
-  echo "Now in folder `pwd`"
+  echo_time "Now in folder `pwd`"
   for FILE in $FILES_TO_CONVERT; do
     if [[ -e $FILE ]]; then
-      echo "Converting $FILE"
+      echo_time "Converting $FILE"
       sed --in-place -E --expression=$SED_EXPRESSION $FILE
       git add $FILE
     fi
   done
 
   DIFF_FILE="../$PROJECT.diff"
-  echo "Logging changes for $PROJECT to $DIFF_FILE"
-  git diff --staged > ../$DIFF_FILE
+  echo_time "Logging changes for $PROJECT to $DIFF_FILE"
+  git diff --staged > $DIFF_FILE
 
-  echo "Finished changes for repo $PROJECT, committing and pushing"
+  echo_time "Finished changes for repo $PROJECT, committing and pushing"
   git commit -F $COMMIT_MESSAGE
   git push
 
@@ -44,5 +51,4 @@ for PROJECT in `tail -r $PROJECT_LIST_FILE | grep -v "^#"`; do
 
 done
 
-# PROJECT="Grocery-Store"
-#
+echo_time "Done!"
